66625b456c4ff7bc1c54cfc5883d21841559bdaf
MagGoldi <stivenH0king@yandex.ru>
2023-05-18 23:16:14 +0400

    add generation

--- files/test.py
+++ files/test.py
@@ -3,6 +3,10 @@ import argparse
 import os  
 import wget 
 from prettytable import PrettyTable  
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 settings = {
@@ -31,10 +35,52 @@ def print_info(text):
 	print('\n')
 	pass
 
-def gen():
+def generation(symmetric_k, public_k, secret_k):
+	print('Длина ключа от 32 до 448 бит с шагом 8 бит')
+	key_len = int(input('Введите длину ключа: '))
+
+	while True:
+		if key_len % 8 != 0 or key_len < 32 or key_len > 448:
+			key_len = int(input('Введите длину ключа: '))
+		else: break
+
+	# генерация пары ключей для асимметричного алгоритма шифрования
+	keys = rsa.generate_private_key(
+		public_exponent=65537,
+		key_size=2048
+	)
+	secret_key = keys
+	public_key = keys.public_key()
+
+	# сериализация открытого ключа в файл
+	with open(public_k, 'wb') as public_out:
+		public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+												 format=serialization.PublicFormat.SubjectPublicKeyInfo))
+	# сериализация закрытого ключа в файл
+	with open(secret_k, 'wb') as secret_out:
+		secret_out.write(secret_key.private_bytes(encoding=serialization.Encoding.PEM,
+													format=serialization.PrivateFormat.TraditionalOpenSSL,
+													encryption_algorithm=serialization.NoEncryption()))
+
+	# открываем файл с симметричным ключом
+	with open(symmetric_k, 'rb') as key_file:
+		key = key_file  # забираем его содержимое в переменную key
+	text = bytes(str(key), 'UTF-8')
+	c_text = public_key.encrypt(text, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+	# перезаписываем в файл с симметричным ключом зашифрованный симметричный ключ
+	with open(symmetric_k, 'wb') as key_file:
+		key_file.write(c_text)
+
+	print(f'Ключи асимметричного шифрования сериализованы по адресу: {public_k}\t{secret_k}')
+	print(f"Ключ симметричного шифрования:\t{symmetric_k}")
 	pass
+
+	
+
 def enc():
 	pass
+
 def dec():
 	pass
 
@@ -42,12 +88,13 @@ def main():
 	while True:
 		if args.mode == "gen":
 			print_info('Запущен режим создания ключей')
+
 			if not os.path.exists('setting.json'):
 				with open('setting.json', 'w') as file:
 					json.dump(settings, file)
-			with open('settings.json', 'w') as json_file:
+			with open('settings.json', 'r') as json_file:
 				settings_data = json.load(json_file)
-			gen()
+			generation(settings_data['symmetric_key'], settings_data['public_key'], settings_data['secret_key'])
 			break
 
 		elif args.mode == "enc":
@@ -55,7 +102,7 @@ def main():
 			if not os.path.exists('setting.json'):
 				with open('setting.json', 'w') as file:
 					json.dump(settings, file)
-			with open('settings.json', 'w') as json_file:
+			with open('settings.json', 'r') as json_file:
 				settings_data = json.load(json_file)	
 			enc()
 			break	
@@ -65,7 +112,7 @@ def main():
 			if not os.path.exists('setting.json'):
 				with open('setting.json', 'w') as file:
 					json.dump(settings, file)
-			with open('settings.json', 'w') as json_file:
+			with open('settings.json', 'r') as json_file:
 				settings_data = json.load(json_file)	
 			dec()
 			break
